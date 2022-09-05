d25661b9b6bb80ad9c2261d2ca1149c5d57f8f8f
Lucas Cisterna <lucas.ncisterna@gmail.com>
2022-09-05 18:01:31 -0300

    functionally project uploaded | TODO: MediaQueries

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ function gameMenu() {
     document.getElementById("initialMenu").style.display = "none";
     document.getElementById("newWordMenu").style.display = "none";
     document.getElementById("gameMenu").style.display = "flex";
+    document.getElementById("wrong-letter").innerHTML = "";
     startGame();
 }
 
@@ -44,30 +45,95 @@ function randomWord() {
 }
 
 //function to split a word into inputs
+let wordArray = [];
 function splitWord(word) {
-    let wordArray = word.split("");
+    wordArray = word.split("");
     let wordLength = wordArray.length;
     let wordInputs = "";
     for (let i = 0; i < wordLength; i++) {
-        wordInputs += `<input type="text" id="letter${i + 1}" class="letters" maxlength="1">`;
+        wordInputs += `<input type="text" id="letter${i + 1}" class="letters" maxlength="0">`;
     }
     document.getElementById("word").innerHTML = wordInputs;
-
-    let letter = document.getElementById("letter1");
-    letter.focus();
 }
 
+//function to reset variables
+function reset() {
+    for (let i = 1; i <= 6; i++) {
+        document.getElementById(`hangman${i}`).style.display = "none";
+    }
+    wordArray = [];
+    wrongLetters = [];
+    greatLetters = [];
+
+}
+
+
 //function to start the game
+let word = "";
 function startGame() {
-    let word = randomWord();
+    reset();
+    word = randomWord();
     splitWord(word);
     console.log(word);
 }
 
+let greatLetters = [];
+function checkLetter(letter) {
+    let letterPosition = wordArray.indexOf(letter);
+    for (let i = 0; i < wordArray.length; i++) {
+        if (letterPosition !== -1) {
+            greatLetters.push(letter);
+            document.getElementById("letter" + (letterPosition + 1)).value = letter;
+            wordArray.splice(letterPosition, 1, " ");
+            letterPosition = wordArray.indexOf(letter);
+            if (greatLetters.length === word.length) {
+                document.getElementById("word").innerHTML = `La palabra era: ${word}`;
+                document.getElementById("wrong-letter").innerHTML = `GANASTE con ${wrongLetters.length} errores`;
+            }
+        } else if (letterPosition === -1 && greatLetters.indexOf(letter) === -1) {
+            checkWrongLetters(letter);
+        }
+    }
+}
+
+let wrongLetters = [];
+function checkWrongLetters(letter) {
+    let wrongLetterPosition = wrongLetters.indexOf(letter);
+    console.log(wrongLetterPosition);
+    if (wrongLetterPosition === -1 && wrongLetters.length < 6) {
+        wrongLetters.push(letter);
+        document.getElementById("wrong-letter").innerHTML = wrongLetters;
+        document.getElementById(`hangman${wrongLetters.length}`).style.display = "block";
+    } else if (wrongLetters.length === 6) {
+        document.getElementById("word").innerHTML = `La palabra era: ${word}`;
+        document.getElementById("wrong-letter").innerHTML = "PERDISTE";
+    }
+}
+
+//input only letters
+function onlyLetters(e) {
+    let key = e.keyCode || e.which;
+    let letter = String.fromCharCode(key).toUpperCase();
+    let regex = /^[A-Z]$/;
+    if (!regex.test(letter)) {
+        e.preventDefault();
+    } else {
+        checkLetter(letter);
+    }
+}
+
 function eventsListener() {
+    //intial menu
     initialMenu();
     document.getElementById("addButton").addEventListener("click", newWordMenu);
     document.getElementById("cancelButton").addEventListener("click", initialMenu);
-    document.getElementById("saveButton").addEventListener("click", (initialMenu, validateWord));
+
+    //new word menu
+    document.getElementById("saveButton").addEventListener("click", () => { initialMenu(); validateWord(); });
     document.getElementById("playButton").addEventListener("click", gameMenu);
+
+    //game menu
+    document.getElementById("word").addEventListener("keypress", onlyLetters);
+    document.getElementById("quitButton").addEventListener("click", initialMenu);
+    document.getElementById("startAgainButton").addEventListener("click", gameMenu);
 }
